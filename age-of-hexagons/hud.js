19c9cd16a90bb74fb432af69eb6f7b8de45d1330
Tomas Balyo <biotomas@gmail.com>
2020-05-14 19:54:26 +0200

    end turn button started

--- age-of-hexagons/hud.js
+++ age-of-hexagons/hud.js
@@ -1,10 +1,10 @@
 hudx = 10;
 hudy = 10;
 hudHeigth = 65;
-hudPerPlayer = 70;
+hudPerPlayer = 80;
 
 function drawHud(cc) {
-    hudWidth = hudPerPlayer + hudPerPlayer*gameMaster.players;
+    hudWidth = hudPerPlayer + hudPerPlayer * gameMaster.players;
 
     cc.beginPath();
     cc.lineWidth = "1";
@@ -23,9 +23,20 @@ function drawHud(cc) {
 
     for (var playerIndex = 1; playerIndex <= gameMaster.players; playerIndex++) {
         if (gameMaster.currentPlayer == playerIndex) {
+            // end turn button
+            cc.beginPath();
+            cc.lineWidth = "1";
+            cc.strokeStyle = "black";
+            cc.fillStyle = "#ddd";
+            cc.rect(hudx + 5 + playerIndex * hudPerPlayer, hudy + hudHeigth + 5,
+                hudPerPlayer, 20);
+            cc.stroke();
+            cc.fill();
+            cc.fillStyle = "black";
+            cc.fillText("End Turn", hudx + 20 + playerIndex * hudPerPlayer, hudy + hudHeigth + 20);
             cc.font = "bold 15px Arial";
         }
-        cc.fillText(playerNames[playerIndex], hudx + 5 + playerIndex*hudPerPlayer, hudy + 15);
+        cc.fillText(playerNames[playerIndex], hudx + 5 + playerIndex * hudPerPlayer, hudy + 15);
         cc.font = "15px Arial";
         var income = gameMaster.playerStates[playerIndex].getIncome();
         if (income > 0) {
@@ -34,11 +45,11 @@ function drawHud(cc) {
         } else {
             cc.fillStyle = "red";
         }
-        cc.fillText(income, hudx + 5 + playerIndex*hudPerPlayer, hudy + 30);
+        cc.fillText(income, hudx + 5 + playerIndex * hudPerPlayer, hudy + 30);
         cc.fillStyle = "black";
-        cc.fillText(gameMaster.playerStates[playerIndex].money, hudx + 5 + playerIndex*hudPerPlayer, hudy + 45);
-        cc.fillText(gameMaster.playerStates[playerIndex].moves, hudx + 5 + playerIndex*hudPerPlayer, hudy + 60);
-    
+        cc.fillText(gameMaster.playerStates[playerIndex].money, hudx + 5 + playerIndex * hudPerPlayer, hudy + 45);
+        cc.fillText(gameMaster.playerStates[playerIndex].moves, hudx + 5 + playerIndex * hudPerPlayer, hudy + 60);
+
     }
 
 
